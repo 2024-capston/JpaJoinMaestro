fd0b19f9f152be0b21bd996e3a9c35d336a84ad1
nookcoder <gus5427@naver.com>
2024-04-08 23:10:59 +0900

    init

---
+++ src/main/java/org/sejong/jpajoinmaestro/JpaJoinMaestroApplication.java
@@ -0,0 +1,13 @@
+package org.sejong.jpajoinmaestro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaJoinMaestroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaJoinMaestroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sejong/jpajoinmaestro/JpaJoinMaestroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sejong.jpajoinmaestro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaJoinMaestroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
