b95b4d75e13057abd7e5457113404f00773e94e7
sungpaks <emforhs03150@gmail.com>
2024-05-24 17:11:08 +0900

    [feat] : 가장 사용 가능성이 높은 인덱스 추출 로직 추가

--- src/main/java/org/sejong/jpajoinmaestro/core/optimizer/internal/WhereClauseOptimizerImpl.java
+++ src/main/java/org/sejong/jpajoinmaestro/core/optimizer/internal/WhereClauseOptimizerImpl.java
@@ -13,9 +13,12 @@ import org.sejong.jpajoinmaestro.core.optimizer.spi.WhereClauseOptimizer;
 import org.sejong.jpajoinmaestro.core.query.clause.Equal;
 import org.sejong.jpajoinmaestro.core.query.clause.More;
 import org.sejong.jpajoinmaestro.core.query.clause.PredicateBuilder;
+import org.sejong.jpajoinmaestro.core.query.constants.CONDITION_FLAG;
 import org.sejong.jpajoinmaestro.core.query.constants.PREDICATE_CONJUNCTION;
 import org.sejong.jpajoinmaestro.domain.OrderDetail;
 import org.sejong.jpajoinmaestro.core.query.clause.*;
+import org.sejong.jpajoinmaestro.domain.Orders;
+import org.sejong.jpajoinmaestro.domain.Shipment;
 
 import java.lang.reflect.Field;
 import java.util.*;
@@ -32,16 +35,15 @@ public class WhereClauseOptimizerImpl implements WhereClauseOptimizer {
 		 *
 		 * TODO?? : 조건절에서 인덱스 컬럼에 별도의 연산을 넣지 않도록 하기? 근데 해줄 수 있는게 있는지는 모르곘음
 		 */
-		PredicateBuilder pb = new PredicateBuilder(new More().than(OrderDetail.class, "Quantity", 100)).and(new Equal().to(OrderDetail.class, "Id", 1));
+		PredicateBuilder pb = new PredicateBuilder(new More().than(Shipment.class, "shipmentStatus", "TRANSIT")).and(new Equal().to(Orders.class, "userId", 1));
 		Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates = pb.getPredicates();
-		getMostPossibleIndex(dtoClass, predicates);
 
-		replaceColumns(predicates);
+		replaceColumns(dtoClass, predicates);
 		//if (isIndexSkipScanNeeded(predicates)) /*index_ss 힌트 추가? */;
 		return null; // TODO : return CQ;
 	}
 
-	private void replaceColumns(Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
+	private void replaceColumns(Class<?> dtoClass, Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
 		/**
 		 * operator 종류와 index 여부에 따라 조건절 재배치 (가중치만 매기기? or List를 정렬?)
 		 * 등치조건을 부등호 조건보다 앞에 오도록 하는 등
@@ -51,19 +53,20 @@ public class WhereClauseOptimizerImpl implements WhereClauseOptimizer {
 		 * 인덱스 후행컬럼이고 부등호면 그 다음
 		 * 인덱스 없으면 상관없겠지?
 		 *
-		 * 	TODO : 카디널리티 또한 고려하여 가중치 매기기
+		 * 	TODO : 카디널리티 또한 고려하여 가중치 매기기?
 		 */
-
-		//TODO : (신규) INDEX가 무엇이 쓰일지 판단..??
+		//먼저 predciates에 대한 가장 가능성 높은 인덱스 추출
+		List<HashMap<Class<?>,ExtractedIndex>> mostPossibleIndexes = getMostPossibleIndex(dtoClass, predicates);
+		for(HashMap<Class<?>,ExtractedIndex> indexes : mostPossibleIndexes) {
+			for(Map.Entry<Class<?>,ExtractedIndex> entry : indexes.entrySet()) {
+				System.out.println(entry.getKey().toString() + "::" + entry.getValue().toString());
+			}
+		}
 		for (HashMap<PREDICATE_CONJUNCTION, Predicate> map : predicates) {
 			for (Map.Entry<PREDICATE_CONJUNCTION, Predicate> entry : map.entrySet()) {
 				PREDICATE_CONJUNCTION key = entry.getKey();
 				Predicate value = entry.getValue();
-				Extractor extractor = new Extractor(entityManager);
-				List<ExtractedIndex> entityIndexes = extractor.getEntityIndexes(value.getDomainClass());
-				for(ExtractedIndex entityIndex : entityIndexes) {
-					System.out.println(entityIndex);
-				}
+
 			}
 		}
 
@@ -120,37 +123,65 @@ public class WhereClauseOptimizerImpl implements WhereClauseOptimizer {
 
 		return false;
 	}
-	private HashMap<Class<?>, ExtractedIndex> getMostPossibleIndex(Class<?> dtoClass, Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
-		Extractor extractor = new Extractor(entityManager);
+
+	/**
+	 * @param dtoClass : 쿼리 결과로 받고싶은 dtoClass
+	 * @param predicates : 쿼리 조건절
+	 * @return : <domainClass, 가장 사용 가능성이 높은 INDEX> 쌍
+	 */
+	private List<HashMap<Class<?>, ExtractedIndex>> getMostPossibleIndex(Class<?> dtoClass, Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
+        Extractor extractor = new Extractor(entityManager);
 		List<Class<?>> domainClasses = new ArrayList<>(); //dtoClass에서 참조하는 domainClass들
 		for(Field f : dtoClass.getDeclaredFields()) { //dtoClass의 각 필드에 대해,
-			Class<?> domainClass = f.getAnnotation(DTOFieldMapping.class).domain(); //그 필드의 domainClass가
-			if (!domainClasses.contains(domainClass)) domainClasses.add(domainClass); //아직 List에 없으면 넣기
+			DTOFieldMapping dtoFieldMapping = f.getAnnotation(DTOFieldMapping.class);
+			if(dtoFieldMapping != null) {
+				Class<?> domainClass = dtoFieldMapping.domain(); //그 필드의 domainClass가
+				if (!domainClasses.contains(domainClass)) domainClasses.add(domainClass); //아직 List에 없으면 넣기
+			}
 		}
 		//이제 dtoClass에서 참조하는 domainClass들을 알아내었음
-		HashMap<Class<?>, ExtractedIndex> mostPossibleEntityIndexes = new HashMap<>();
+		List<HashMap<Class<?>, ExtractedIndex>> mostPossibleEntityIndexes = new ArrayList<>();
 		for(Class<?> domainClass : domainClasses) {
 			//각 domainClass의 인덱스들을 확인
-			int max_score = 0;
+			double max_score = 0;
 			ExtractedIndex bestIndex = null;
 			for(ExtractedIndex entityIndex : extractor.getEntityIndexes(domainClass)) {
-				int score = 0;
-				//PK거나 UniqueIndex
-				if (entityIndex.getIsPrimaryKey() || entityIndex.getUnique())
-					score+=10;
+				double score = 0;
+				/* PK거나 UniqueIndex이면서 && (조회 컬럼에 포함?)*/
+				if ((entityIndex.getIsPrimaryKey() || entityIndex.getUnique()))
+					score+=5; //5 or 10
 
-				// WHERE절에서 사용되는 컬럼임?
+				/* WHERE절에서 사용되는 컬럼임? + 얼마나 선행컬럼임?? */
 				for (HashMap<PREDICATE_CONJUNCTION, Predicate> predicate : predicates) {
 					for (Map.Entry<PREDICATE_CONJUNCTION, Predicate> entry : predicate.entrySet()) {
 						Predicate value = entry.getValue();
 						PREDICATE_CONJUNCTION key = entry.getKey();
-						if (entityIndex.getColumnList().contains(value.getFieldName())) {
-
-						}
+						score += entityIndex.getIndexWeightOfColumn(value.getFieldName()) * 10;
+						/* LIKE "%어쩌구"같이 %가 앞에 오면 index타기 힘드니까 가중치 빼기 */
+						if (value.getFlag().equals(CONDITION_FLAG.LIKE) && ((Like) value).getValue().toString().startsWith("%"))
+							score -= 4;
 					}
 				}
+
+				/* ORDER BY절에서 사용되는 컬럼임? + 얼마나 선행컬럼? */
+				/* TODO: ORDER BY 아직 없음 */
+
+				/* GROUP BY절에서 사용되는 컬럼임? + 얼마나 선행 컬럼? */
+				/* TODO: GROUP BY 아직 없음 */
+
+				/* JOIN에 사용되나? */
+				/* TODO : JOIN의 ON절 참조 */
+				System.out.println(domainClass + "=>" + entityIndex.toString() + "=>" + "index possibility : " + score);
+				if (score > max_score) { //점수가 높으면 bestIndex 갱신
+					max_score = score;
+					bestIndex = entityIndex;
+				}
 			}
+			HashMap<Class<?>, ExtractedIndex> domainAndIndex = new HashMap<>();
+			domainAndIndex.put(domainClass, bestIndex); //bestIndex로 <domainClass-ExtractedIndex>쌍 생성
+			mostPossibleEntityIndexes.add(domainAndIndex); //리스트에 추가
 		}
-		return null;
+		System.out.println("done.");
+		return mostPossibleEntityIndexes;
 	}
 }
