55c1b2a8f9601a7134997d92d084cf55f6d98fef
sungpaks <emforhs03150@gmail.com>
2024-05-24 16:17:02 +0900

    [fix] : 인덱스 선행후행컬럼 가중치는 0~1값을 가지도록 변경

--- src/main/java/org/sejong/jpajoinmaestro/core/extractor/domain/ExtractedIndex.java
+++ src/main/java/org/sejong/jpajoinmaestro/core/extractor/domain/ExtractedIndex.java
@@ -35,9 +35,14 @@ public class ExtractedIndex {
         return isPrimaryKey;
     }
 
-    public int getIndexWeightOfColumn(String column) {
-        //추가 : 해당 컬럼이 인덱스에 포함이면 (전체 사이즈 - 컬럼의 인덱스 순서)로 선행컬럼이 더 높은 값을 가지게 하며, 미포함이면 0
-        return columnList.contains(column) ? columnList.length() - columnList.indexOf(column) : 0;
+    public double getIndexWeightOfColumn(String column) {
+        /**
+         * 추가 : 해당 컬럼이 인덱스에 포함이면
+         * (전체 사이즈 - 컬럼의 인덱스 순서)/(전체 사이즈로) 선행컬럼이 더 높은 값을 가지게 하며,
+         * 미포함이면 0
+         * finally, 가중치가 0.5보다 크면 선행컬럼이라고 볼 수 있겠음
+         */
+        return columnList.contains(column) ? (double) (columnList.length() - columnList.indexOf(column)) / columnList.length() : 0;
     }
 
     @Override
