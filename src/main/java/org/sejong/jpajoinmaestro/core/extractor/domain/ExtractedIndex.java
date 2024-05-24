820865dba7789d0e38bd0d7c417897a9b03f3234
sungpaks <emforhs03150@gmail.com>
2024-05-24 16:06:25 +0900

    getIndexWeightOfColumn 메서드 추가 : 어떤 컬럼이 특정 인덱스에서 얼마나 선행컬럼인지 가중치를 알기 위함

--- src/main/java/org/sejong/jpajoinmaestro/core/extractor/domain/ExtractedIndex.java
+++ src/main/java/org/sejong/jpajoinmaestro/core/extractor/domain/ExtractedIndex.java
@@ -35,6 +35,11 @@ public class ExtractedIndex {
         return isPrimaryKey;
     }
 
+    public int getIndexWeightOfColumn(String column) {
+        //추가 : 해당 컬럼이 인덱스에 포함이면 (전체 사이즈 - 컬럼의 인덱스 순서)로 선행컬럼이 더 높은 값을 가지게 하며, 미포함이면 0
+        return columnList.contains(column) ? columnList.length() - columnList.indexOf(column) : 0;
+    }
+
     @Override
     public String toString() {
         return "ExtractedIndex{" +
