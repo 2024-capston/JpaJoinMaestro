058ca68007f06be2d68567b547e38a0e5027fba9
sungpaks <emforhs03150@gmail.com>
2024-05-24 18:58:22 +0900

    [Feat] : replacePredicates, likeToBetween 메서드 로직 작성

--- src/main/java/org/sejong/jpajoinmaestro/core/optimizer/internal/WhereClauseOptimizerImpl.java
+++ src/main/java/org/sejong/jpajoinmaestro/core/optimizer/internal/WhereClauseOptimizerImpl.java
@@ -3,9 +3,7 @@ package org.sejong.jpajoinmaestro.core.optimizer.internal;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.CriteriaQuery;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.sejong.jpajoinmaestro.core.annotations.DTOFieldMapping;
 import org.sejong.jpajoinmaestro.core.extractor.Extractor.Extractor;
 import org.sejong.jpajoinmaestro.core.extractor.domain.ExtractedIndex;
@@ -15,7 +13,6 @@ import org.sejong.jpajoinmaestro.core.query.clause.More;
 import org.sejong.jpajoinmaestro.core.query.clause.PredicateBuilder;
 import org.sejong.jpajoinmaestro.core.query.constants.CONDITION_FLAG;
 import org.sejong.jpajoinmaestro.core.query.constants.PREDICATE_CONJUNCTION;
-import org.sejong.jpajoinmaestro.domain.OrderDetail;
 import org.sejong.jpajoinmaestro.core.query.clause.*;
 import org.sejong.jpajoinmaestro.domain.Orders;
 import org.sejong.jpajoinmaestro.domain.Shipment;
@@ -35,79 +32,92 @@ public class WhereClauseOptimizerImpl implements WhereClauseOptimizer {
 		 *
 		 * TODO?? : 조건절에서 인덱스 컬럼에 별도의 연산을 넣지 않도록 하기? 근데 해줄 수 있는게 있는지는 모르곘음
 		 */
-		PredicateBuilder pb = new PredicateBuilder(new More().than(Shipment.class, "shipmentStatus", "TRANSIT")).and(new Equal().to(Orders.class, "userId", 1));
+		PredicateBuilder pb = new PredicateBuilder(new More().than(Shipment.class, "shipmentStatus", "TRANSIT")).and(new Equal().to(Orders.class, "userId", 1)).and(new Like().with(Orders.class, "status", "DONE%"));
 		Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates = pb.getPredicates();
 
-		replaceColumns(dtoClass, predicates);
+		likeToBetween(predicates);
+		replacePredicates(dtoClass, predicates);
 		//if (isIndexSkipScanNeeded(predicates)) /*index_ss 힌트 추가? */;
-		return null; // TODO : return CQ;
+		return null;
 	}
 
-	private void replaceColumns(Class<?> dtoClass, Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
-		/**
-		 * operator 종류와 index 여부에 따라 조건절 재배치 (가중치만 매기기? or List를 정렬?)
-		 * 등치조건을 부등호 조건보다 앞에 오도록 하는 등
-		 * 인덱스 선행컬럼이고 등치조건이면 앞쪽으로
-		 * 인덱스 선행컬럼인데 부등호면 그 다음
-		 * 인덱스 후행컬럼이고 등치조건이면 그 다음
-		 * 인덱스 후행컬럼이고 부등호면 그 다음
-		 * 인덱스 없으면 상관없겠지?
-		 *
-		 * 	TODO : 카디널리티 또한 고려하여 가중치 매기기?
-		 */
-		//먼저 predciates에 대한 가장 가능성 높은 인덱스 추출
-		List<HashMap<Class<?>,ExtractedIndex>> mostPossibleIndexes = getMostPossibleIndex(dtoClass, predicates);
-		for(HashMap<Class<?>,ExtractedIndex> indexes : mostPossibleIndexes) {
-			for(Map.Entry<Class<?>,ExtractedIndex> entry : indexes.entrySet()) {
-				System.out.println(entry.getKey().toString() + "::" + entry.getValue().toString());
-			}
-		}
+	/**
+	 *
+	 * @param dtoClass : 쿼리 결과로 받을 dtoClass
+	 * @param predicates : 쿼리 조건절 predicates
+	 *                   predicates의 각 predicate 객체들을 효율적으로 재배치
+	 */
+	private void replacePredicates(Class<?> dtoClass, Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
+		//먼저 predciates에 대한 가장 가능성 높은 인덱스들 추출
+		HashMap<Class<?>,ExtractedIndex> mostLikelyIndexes = getMostLikelyIndexes(dtoClass, predicates);
+
 		for (HashMap<PREDICATE_CONJUNCTION, Predicate> map : predicates) {
 			for (Map.Entry<PREDICATE_CONJUNCTION, Predicate> entry : map.entrySet()) {
 				PREDICATE_CONJUNCTION key = entry.getKey();
 				Predicate value = entry.getValue();
 
+				ExtractedIndex index = mostLikelyIndexes.get(value.getDomainClass());
+				//현재 Predicate의 도메인 클래스에 대한 mostLikelyIndex 추출
+
+				double weight = 0;
+				System.out.println(index.toString() + "/" + value.getDomainClass() + "/" + value.getFieldName());
+				if (value.getFlag().equals(CONDITION_FLAG.EQUAL)) {
+					weight += 10 * index.getIndexWeightOfColumn(value.getFieldName());
+				} //등치조건이면 10*인덱스가중치, 아니면 5*인덱스가중치
+				else {
+					weight += 5 * index.getIndexWeightOfColumn(value.getFieldName());
+				}
+				value.setWeight(weight);
 			}
 		}
+		/*
+		* TODO : 잘 재배치 되는지 검증 필요.
+		* 필드 명과 실제 컬럼 이름 간의 차이때문에 0으로 getIndexWeightOfColumn이 잘 동작하지 않을 수도?
+		* 또는 인덱스가 제대로 찾아졌는지도?
+		* */
 
-		/*for (Condition condition : conditions) {
-			int weight = 0;
-			if (condition.getIndexWeight() > 0) {
-				// 인덱스 컬럼일 경우
-				weight = condition.getIndexWeight() * 10;
-				if (condition.getOperatorType() == Condition.OperatorType.EQUAL) {
-					weight += 5; // EQUAL 조건일 경우 추가 가중치
-				}
+		/* TODO : PriorityQueue로 Predicates를 담거나, 아예 ArrayList로 담아서 정렬하던지, ..  + CONJUNCTION도 바꿔야 함??*/
+		PriorityQueue<Predicate> sortedPredicates = new PriorityQueue<>((p1, p2)->(int)(p1.getWeight()-p2.getWeight()));
+		for(HashMap<PREDICATE_CONJUNCTION, Predicate> map : predicates) {
+			for (Map.Entry<PREDICATE_CONJUNCTION, Predicate> entry : map.entrySet()) {
+				sortedPredicates.add(entry.getValue());
 			}
-			condition.setWeight(weight);
-		}*/
-
-		// 가중치를 기준으로 내림차순 정렬
-		//conditions.sort((c1, c2) -> Integer.compare(c2.getWeight(), c1.getWeight()));
+		}
+		for (Predicate predicate : sortedPredicates) {
+			System.out.println( predicate.getDomainClass() + ":" + predicate.getFieldName() + ":" + predicate.getFlag() + "=>" + predicate.getWeight());
+		}
 
 		return;
 	}
+
+	/**
+	 *
+	 * @param predicates : predicate 객체들 중 LIKE "어쩌고%" 이면 between으로 바꾼다.
+	 *                   TODO : 숫자형일 때도 바꿔주는게 좋을지도?
+	 */
 	private void likeToBetween(Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
-		/**
-		 * Like --> Between. 바꿀 수 있으면 바꾸기
-		 * - 접두사 매칭 (LIKE "SEOUL%"같은)
-		 *
-		 */
-		/*for (int i = 0; i < conditions.size(); i++) {
-			Condition<?> condition = conditions.get(i);
+		for (HashMap<PREDICATE_CONJUNCTION, Predicate> map : predicates) {
+			for (Map.Entry<PREDICATE_CONJUNCTION, Predicate> entry : map.entrySet()) {
+				Predicate predicate = entry.getValue();
+				if (!predicate.getFlag().equals(CONDITION_FLAG.LIKE)) continue;
 
-			if (condition.getOperatorType() == Condition.OperatorType.LIKE && condition.getValue() instanceof String) {
-				String value = (String) condition.getValue(); //LIKE연산 + value가 String
+				Object likeValue = ((Like)predicate).getValue();
+				if (predicate.getFlag() == CONDITION_FLAG.LIKE && likeValue instanceof String) {
+					String value = (String) likeValue; // LIKE 연산 + value가 String
 
-				if (!value.startsWith("%") && value.endsWith("%")) { //접두사 매칭일 때
-					String prefix = value.substring(0, value.length() - 1);
-					String startValue = prefix;
-					String endValue = prefix + Character.MAX_VALUE;
+					if (!value.startsWith("%") && value.endsWith("%")) { // 접두사 매칭일 때
+						String prefix = value.substring(0, value.length() - 1);
+						String startValue = prefix;
+						String endValue = prefix + Character.MAX_VALUE;
 
-					conditions.set(i, new Condition<>(Condition.OperatorType.BETWEEN, condition.getColumnName(), new String[] {startValue, endValue}, condition.getIndexWeight()));
+						// BETWEEN 조건으로 변경
+						Predicate betweenPredicate = new Between().between(predicate.getDomainClass(), predicate.getFieldName(), startValue, endValue);
+						entry.setValue(betweenPredicate);
+					}
 				}
 			}
-		}*/
+		}
+		// between으로 성공적으로 바뀌는 것은 확인함. TODO : 실제 쿼리 결과도 똑같은지 확인해야.
 	}
 	private boolean isIndexSkipScanNeeded(Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
 		/**
@@ -129,7 +139,7 @@ public class WhereClauseOptimizerImpl implements WhereClauseOptimizer {
 	 * @param predicates : 쿼리 조건절
 	 * @return : <domainClass, 가장 사용 가능성이 높은 INDEX> 쌍
 	 */
-	private List<HashMap<Class<?>, ExtractedIndex>> getMostPossibleIndex(Class<?> dtoClass, Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
+	private HashMap<Class<?>, ExtractedIndex> getMostLikelyIndexes(Class<?> dtoClass, Queue<HashMap<PREDICATE_CONJUNCTION, Predicate>> predicates) {
         Extractor extractor = new Extractor(entityManager);
 		List<Class<?>> domainClasses = new ArrayList<>(); //dtoClass에서 참조하는 domainClass들
 		for(Field f : dtoClass.getDeclaredFields()) { //dtoClass의 각 필드에 대해,
@@ -139,8 +149,16 @@ public class WhereClauseOptimizerImpl implements WhereClauseOptimizer {
 				if (!domainClasses.contains(domainClass)) domainClasses.add(domainClass); //아직 List에 없으면 넣기
 			}
 		}
+		/*
+		검증테스트
+		for (Class<?> domainClass : domainClasses) {
+			List<ExtractedIndex> indexes = extractor.getEntityIndexes(domainClass);
+			for (ExtractedIndex index : indexes) {
+				System.out.println(domainClass.toString() + " " + index.toString());
+			}
+		}*/
 		//이제 dtoClass에서 참조하는 domainClass들을 알아내었음
-		List<HashMap<Class<?>, ExtractedIndex>> mostPossibleEntityIndexes = new ArrayList<>();
+		HashMap<Class<?>, ExtractedIndex> mostPossibleEntityIndexes = new HashMap<>();
 		for(Class<?> domainClass : domainClasses) {
 			//각 domainClass의 인덱스들을 확인
 			double max_score = 0;
@@ -171,17 +189,13 @@ public class WhereClauseOptimizerImpl implements WhereClauseOptimizer {
 
 				/* JOIN에 사용되나? */
 				/* TODO : JOIN의 ON절 참조 */
-				System.out.println(domainClass + "=>" + entityIndex.toString() + "=>" + "index possibility : " + score);
 				if (score > max_score) { //점수가 높으면 bestIndex 갱신
 					max_score = score;
 					bestIndex = entityIndex;
 				}
 			}
-			HashMap<Class<?>, ExtractedIndex> domainAndIndex = new HashMap<>();
-			domainAndIndex.put(domainClass, bestIndex); //bestIndex로 <domainClass-ExtractedIndex>쌍 생성
-			mostPossibleEntityIndexes.add(domainAndIndex); //리스트에 추가
+			mostPossibleEntityIndexes.put(domainClass, bestIndex); //리스트에 추가
 		}
-		System.out.println("done.");
 		return mostPossibleEntityIndexes;
 	}
 }
